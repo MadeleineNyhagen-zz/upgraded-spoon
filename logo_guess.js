6b246abf18b5cdf4c361813ef592401b83ef5dfc
Chris <chris.wiseman74@gmail.com>
2016-07-06 11:15:58 -0700

    Updated javascript rough draft

---
+++ logo_guess.js
@@ -0,0 +1,65 @@
+// JavaScript Document
+
+function yourGuess() {
+    // You can store references to the value and the 
+    // guesses textarea in local function variables.
+    var guess = document.getElementById("guess").value;
+    var guesses = document.getElementById("output");
+
+    // First, if the guess is the same, just show the answer.
+    // Append onto the textarea the result.
+    if (guess == logoToGuess) {
+        guesses.value = guesses.value + "\r" + "You have guessed correctly! ("+guess+")";
+    } else {
+        guesses.value = guesses.value + "\r" + "You guessing incorrectly!("+guess+")";
+    }
+}
+    
+function showLogoToGuess() {
+    // Show the randomly generated number if the onclick event
+    // fires and the checkbox is check; otherwise, remove the
+    // number and hide using display: none;.
+    if (document.getElementById('cheat').checked) {
+        document.getElementById('logoToGuess').value = logoToGuess;
+     
+    } else {
+        document.getElementById('logoToGuess').value = '';
+        document.getElementById('cheatShow').style.display = 'none';
+    }
+}
+
+// Randomly generate a number
+function generateNumberToGuess(confirmIt) {
+    var guesses = document.getElementById("output");
+    
+    // First, confirm this is what we want if the confirmIt
+    // argument was passed.
+    if (confirmIt && !confirm('Restart game with new number?')) {
+        return;
+    }
+    
+    guesses.value = '';
+    logoToGuess = Math.floor(Math.random()*500);
+    guesses.value = "New number generated.\n";
+    
+    // Don't forget to hide the new number.
+    document.getElementById('logoToGuess').value = '';
+    document.getElementById('cheatShow').style.display = 'none';
+}
+
+function showGuesses(){
+    var guesses = document.getElementById('guesses');
+    var btn = document.getElementById('showguesses');
+    
+    if (guesses.style.display != 'block') {
+        guesses.style.display = 'block';
+        btn.value = 'Hide My Guesses';
+    } else {
+        guesses.style.display = 'none';
+        btn.value = 'Show My Guesses';
+    }
+}
+
+window.onload = function(){
+    generateNumberToGuess();
+}
