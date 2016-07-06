467e0c825b48ed1c9672876bf06503573558b597
Madeleine Nyhagen <m.l.nyhagen@gmail.com>
2016-07-06 12:09:04 -0700

    updated

--- logo_guess.js
+++ logo_guess.js
@@ -1,27 +1,34 @@
 // JavaScript Document
 
-function yourGuess() {
-    // You can store references to the value and the 
+function yourGuess(input, answer) {
+    // You can store references to the value and the
     // guesses textarea in local function variables.
-    var guess = document.getElementById("guess").value;
+
+
+    var guess = document.getElementById(input).value;
+    var lowerguess = guess.toLowerCase();
+
+
     var guesses = document.getElementById("output");
 
     // First, if the guess is the same, just show the answer.
     // Append onto the textarea the result.
-    if (guess == logoToGuess) {
-        guesses.value = guesses.value + "\r" + "You have guessed correctly! ("+guess+")";
+    if (lowerguess == answer) {
+        alert("You're correct!");
+        //guesses.value = guesses.value + "\r" + "You have guessed correctly! ("+answer+")";
     } else {
-        guesses.value = guesses.value + "\r" + "You guessing incorrectly!("+guess+")";
+        alert("You're incorrect!")
+        guesses.value = guesses.value + "\r" + "You guessing incorrectly!("+answer+")";
     }
 }
-    
+
 function showLogoToGuess() {
     // Show the randomly generated number if the onclick event
     // fires and the checkbox is check; otherwise, remove the
     // number and hide using display: none;.
     if (document.getElementById('cheat').checked) {
         document.getElementById('logoToGuess').value = logoToGuess;
-     
+
     } else {
         document.getElementById('logoToGuess').value = '';
         document.getElementById('cheatShow').style.display = 'none';
@@ -31,16 +38,16 @@ function showLogoToGuess() {
 // Randomly generate a number
 function generateNumberToGuess(confirmIt) {
     var guesses = document.getElementById("output");
-    
+
     // First, confirm this is what we want if the confirmIt
     // argument was passed.
     if (confirmIt && !confirm('Restart game with new logo?')) {
         return;
     }
-    
+
     guesses.value = '';
     guesses.value = "New logo generated.\n";
-    
+
     // Don't forget to hide the new number.
     document.getElementById('logoToGuess').value = '';
     document.getElementById('cheatShow').style.display = 'none';
@@ -49,7 +56,7 @@ function generateNumberToGuess(confirmIt) {
 function showGuesses(){
     var guesses = document.getElementById('guesses');
     var btn = document.getElementById('showguesses');
-    
+
     if (guesses.style.display != 'block') {
         guesses.style.display = 'block';
         btn.value = 'Hide My Guesses';
